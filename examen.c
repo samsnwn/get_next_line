8265203ab181930eb043b80230eb8fdf5f4cc2c4
samsnwn <samuel.c.glantz@gmail.com>
2025-02-18 14:28:15 +0100

    continue exam prep

--- examen.c
+++ examen.c
@@ -5,7 +5,7 @@
 #  define BUFFER_SIZE 1000000
 # endif
 
-char    *read_to_buffer(int fd, char *buffer)
+static char    *read_to_buffer(int fd, char *buffer)
 {
     char *temp;
     int bytes_read;
@@ -16,12 +16,52 @@ char    *read_to_buffer(int fd, char *buffer)
     bytes_read = 1;
     while (!ft_strchr(buffer, '\n') && bytes_read > 0)
     {
-        bytes_read = read(fd, buffer, BUFFER_SIZE)
+        bytes_read = read(fd, temp, BUFFER_SIZE);
+        if (bytes_read == -1)
+        {
+            free(temp);
+            free(buffer);
+            return (NULL);
+        }
+        temp[bytes_read] = '\0';
+        buffer = ft_strjoin(buffer, temp);
+        if (!buffer)
+            return (NULL);
     }
+    free(temp);
     return (buffer);
 }
 
-char	*get_next_line(int fd)
+static char    *extract_line(char *buffer)
+{
+    char *line;
+    int i;
+
+    if (!buffer[i])
+        return (NULL);
+    while (buffer[i] && buffer[i] != '\n')
+        i++;
+    line = malloc(sizeof(char) * (i + 2));
+    if (!line)
+        return (NULL);
+    i = 0;
+    while (buffer[i] && buffer[i] != '\n')
+    {
+        line[i] = buffer[i];
+        i++;
+    }
+    if (buffer[i] == '\n')
+        line[i++] = '\n';
+    line[i] = '\0';
+    return (line);
+}
+
+static char *update_buffer(char *buffer)
+{
+    
+}
+
+static char	*get_next_line(int fd)
 {
     static char *buffer;
     char *line;
